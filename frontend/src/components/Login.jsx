deff239edc7e426211fc814589d73c32d30945cf
sayedgouse <sayedgouse1995@gmail.com>
2024-01-18 13:35:28 +0530

    changes

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -3,7 +3,9 @@ import React from 'react'
 const Login = () => {
   return (
     <div>
-      Login
+        <p className="text-red-500">Red Text</p>
+      <p className="text-blue-700">Blue Text</p>
+      <p className="text-green-600">Green Text</p>
     </div>
   )
 }
